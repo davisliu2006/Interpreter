c60e3d0d82c01245dc309c70c7fd6606fddfe0f7
Davis Liu <davisliu2006@gmail.com>
2026-04-15 10:44:39 -0400

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include <bits/stdc++.h>
-#include "tokenizer.hpp"
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cout << setprecision(15);
-
-    compiler::tokenizer::init();
-    auto tokens = compiler::tokenizer::tokenize("int x = x0 0 x;");
-    for (const auto& [type, token]: tokens) {
-        cout << type << ' ' << token << '\n';
-    }
 
     return 0;
 }
